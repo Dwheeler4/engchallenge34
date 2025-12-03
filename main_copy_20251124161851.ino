e70b4842f8ec65645751bded320db50bdb11a6b9
Daniel Wheeler <zcabddw@ucl.ac.uk>
2025-12-03 14:03:44 +0000

    Added initialisation for I2C in main
    Added Wire library initialization and updated loop logic.

--- main_copy_20251124161851.ino
+++ main_copy_20251124161851.ino
@@ -1,12 +1,20 @@
 #include <Arduino.h>
 #include "UiCom.h"
+#include <Wire.h>
 
 void setup() {
+  Wire.begin();
   Serial.begin(115200);
   initUI();
 }
 
-void loop() {
+void loop() {  
+  sendSetpoints();
+  delay(10);
+  readMeasurements();
+
+  // update UI
   handleUI();
-  delay(500); // Send telemetry every 5 seconds
+
+  delay(500);
 }
