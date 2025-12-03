e4445376a80904d94bbae481905239db8b37e868
Daniel Wheeler <zcabddw@ucl.ac.uk>
2025-12-03 12:10:37 +0000

    Reorder BioreactorState initialization parameters

--- StateHolder.cpp
+++ StateHolder.cpp
@@ -1,10 +1,10 @@
 #include "StateHolder.h"
 
 BioreactorState reactorState = {
-    7.0,    // pH setpoint
-    100.0,  // RPM setpoint
-    37.0,   // Temp setpoint
-    0.0,    // currentPH
-    0.0,    // currentRPM
-    0.0     // currentTemp
+    7.0,
+    37.0,
+    100.0,
+    0.0,
+    0.0,
+    0.0
 };
